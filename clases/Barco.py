37667ecbf4358f2adbaf670e056f00b38a4d0e0d
lmiel <lucia.mielgo@gmail.com>
2023-03-22 14:17:34 +0100

    updates class barco

--- clases/Barco.py
+++ clases/Barco.py
@@ -4,8 +4,13 @@ from clases import Case
 from itertools import product, repeat
 from random import choice
 
-from juego import HORIZONTAL, LONGITUDES_BARCOS, ORIENTACIONES
+# from juego import HORIZONTAL, LONGITUDES_BARCOS, ORIENTACIONES
 
+HORIZONTAL = 0
+VERTICAL = 1
+LONGITUDES_BARCOS = [2, 3, 3, 4, 4, 5]
+
+ORIENTACIONES = (VERTICAL, HORIZONTAL)
 
 instances = []
 casillas_ocupadas = set()
@@ -15,61 +20,55 @@ num_columnas = Conventions.tablero_num_columnas
 num2l = Conventions.generar_num_linea
 num2c = Conventions.generar_num_columna
 
+class barco:
+    casillas_ocupadas = set()
+    def __init__(self, longitud):
+        self.longitud = longitud
+        self.orientacion = choice(ORIENTACIONES)
+        self.tocado = False
+        self.hundido = False
 
-def __init__(self, longitud):
-    self.longitud = longitud
-    self.orientacion = choice(ORIENTACIONES)
-    self.tocado = False
-    self.hundido = False
-
-def horizontal(self):
-    if self.orientacion == HORIZONTAL:
-        rang = choice(range(num_lineas))
-        primero = choice(range(num_columnas + 1 - LONGITUDES_BARCOS))
-        letra = num2l(rang)
-        cifras = [num2c(x) for x in range(primero, primero + LONGITUDES_BARCOS)]
-        self.casillas = {Case.instances[l + c]
-                             for l, c in product(repeat(letra, LONGITUDES_BARCOS), cifras)}
-    else:
-        rang = choice(range(num_columnas))
-        primero = choice(range(num_lineas + 1 - LONGITUDES_BARCOS))
-        cifra = num2c(rang)
-        letras = [num2l(x) for x in range(primero, primero + LONGITUDES_BARCOS)]
-
-        # Crear el barco
-        self.casillas = {Case.instances[l + c]
-            for l, c in product(letras, repeat(cifra, LONGITUDES_BARCOS))}
-    return self.casillas
-
-
-def instanciar(self):
-    for existente in instances:
-        if self.casillas.intersection(existente.casillas):
-            # break relativo al "for existente in barcos:"
-            break
+    def horizontal(self):
+        if self.orientacion == HORIZONTAL:
+            rang = choice(range(num_lineas))
+            primero = choice(range(num_columnas + 1 - self.longitud))
+            letra = num2l(rang)
+            cifras = [num2c(x) for x in range(primero, primero + self.longitud)]
+            self.casillas = {Case.Case.instances[l + c] 
+                for l, c in product(repeat(letra, self.longitud), cifras)}
         else:
+            rang = choice(range(num_columnas))
+            primero = choice(range(num_lineas + 1 - self.longitud))
+            cifra = num2c(rang)
+            letras = [num2l(x) for x in range(primero, primero + self.longitud)]
+
+            # Crear el barco
+            self.casillas = {Case.instances[l + c]
+                for l, c in product(letras, repeat(cifra, self.longitud))}
+        return self.casillas
+
+    def instanciar(self):
+        inst = True
+        for existente in instances:
+            if self.casillas.intersection(existente.casillas):
+                inst = False
+                break
+        if inst:
             # Agregar el barco en el contenedor de barcos
             instances.append(self)
             # Informar la casilla que contiene un barco.
             for casilla in self.casillas:
                 casilla.contiene_barco = True
             # Agregar estas casillas a las casillas ocupadas :
-            casillas_ocupadas.update(self.casillas)
-            # break relativo al "for existente in barcos:"
-            break
-    else:
-        # break relativo al "while True:"
-        return
-    
-    
-def generar_barcos(self):
+            barco.casillas_ocupadas.update(self.casillas)
+            """print("Barco en: " + str(self.casillas))"""
+                
+      
+    def generar_barcos(self):
         while True:
-            self.longitud = choice(Conventions.barcos_longitud)
-            self.orientacion = choice(ORIENTACIONES)
-            self.tocado = False
-            self.hundido = False
-            self.horizontal()
-            self.instanciar()
+            barco = barco(choice(Conventions.barcos_longitud))
+            barco.horizontal()
+            barco.instanciar()
             if len(instances) == len(Conventions.barcos_longitud):
                 break
 
